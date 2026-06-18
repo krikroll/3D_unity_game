efd0902a7a4c686d6c4e042ecfe27794b392716a
krikroll <mscibak0@gmail.com>
2026-06-18 20:25:05 +0200

    game

---
+++ 3D game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject prefab;
+    public float sensitivity = 250f;
+
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpPower = 10f;
+    
+    private float mouseX = 0;
+    private float mouseY = 0;
+    GameObject playerCamera;
+    Transform feet;
+    Rigidbody rb;
+    [SerializeField] LayerMask groundLayer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerCamera = GameObject.Find("Main Camera");
+        feet = transform.Find("Feet");
+        Debug.Log(feet);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = new Vector3(0, 2, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(prefab);
+        }
+        float moveX = 0;
+        float moveY = 0;
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1;
+        }
+        
+        float yaw = playerCamera.transform.eulerAngles.y;
+        Vector3 forward = Quaternion.Euler(0, yaw, 0) * Vector3.forward;
+        Vector3 right = Quaternion.Euler(0, yaw, 0) * Vector3.right;
+        
+        // Move!
+        Vector3 moveDirection = (forward * moveY + right * moveX) * moveSpeed;
+        rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.CheckSphere(feet.position, 0.2f, groundLayer))
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpPower, rb.linearVelocity.z);
+        }
+
+        mouseX += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        mouseY -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+        mouseY = Math.Clamp(mouseY, -80, 80);
+        
+        playerCamera.transform.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+}
